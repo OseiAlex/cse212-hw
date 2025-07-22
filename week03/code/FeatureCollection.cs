d3048da59da3389dbceef2a7a660b31a12542adb
OseiAlex <[dextroalex@gmail.com]>
2025-07-22 12:04:51 +0000

    `Updated code for week 03 problems, including FeatureCollection.cs, Maze.cs, and SetsAndMaps.cs.`

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,5 +1,22 @@
+using System.Text.Json.Serialization;
+
 public class FeatureCollection
 {
-    // TODO Problem 5 - ADD YOUR CODE HERE
-    // Create additional classes as necessary
+    [JsonPropertyName("features")]
+    public EarthquakeFeature[] Features { get; set; }
+}
+
+public class EarthquakeFeature
+{
+    [JsonPropertyName("properties")]
+    public FeatureProperties Properties { get; set; }
+}
+
+public class FeatureProperties
+{
+    [JsonPropertyName("place")]
+    public string Place { get; set; }
+
+    [JsonPropertyName("mag")]
+    public double? Mag { get; set; }
 }
