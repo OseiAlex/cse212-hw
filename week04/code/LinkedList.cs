d2e732682e8e69bfee1cde04b8ba05b1742fdeb0
OseiAlex <[dextroalex@gmail.com]>
2025-07-26 21:40:40 +0000

    "Renamed method parameter type in IntArrayExtensionMethods and added newline at end of file."

--- week04/code/LinkedList.cs
+++ week04/code/LinkedList.cs
@@ -230,8 +230,10 @@ public class LinkedList : IEnumerable<int>
     }
 }
 
-public static class IntArrayExtensionMethods {
-    public static string AsString(this IEnumerable array) {
+public static class IntArrayExtensionMethods
+{
+    public static string AsString(this IEnumerable array)
+    {
         return "<IEnumerable>{" + string.Join(", ", array.Cast<int>()) + "}";
     }
 }
